59287a6069b8bbd61e42b675b80badc51e8809c5
Ashley <35859215+AshleyEM@users.noreply.github.com>
2022-09-06 19:21:17 -0700

    Create just_detatch.py

---
+++ just_detatch.py
@@ -0,0 +1,10 @@
+# Completely detatches a selected mesh from an armature
+import bpy
+
+obj = bpy.context.active_object # selected object
+
+if obj.type == 'MESH' and obj.parent != None:
+    if obj.parent.type == 'ARMATURE':
+        obj.parent = None # clear object's parent
+        obj.vertex_groups.clear() # delete object's vertex groups
+        obj.modifiers.remove(obj.modifiers.get('Armature'))# delete object's armature modifier
